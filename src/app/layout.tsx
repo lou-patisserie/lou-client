663c707fae47e77d860e0ca482d45d1fb2d3f09d
itsaFan <steffansim@gmail.com>
2024-03-24 15:50:11 +0700

    setup shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,8 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={GeistSans.variable}>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={cn(GeistSans.variable)}>{children}</body>
     </html>
   );
 }
+// className={GeistSans.variable}

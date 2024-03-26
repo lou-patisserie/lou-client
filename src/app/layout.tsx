cbc0fe4d3578de735e36acf95cc372bfa2bcfcd5
itsaFan <steffansim@gmail.com>
2024-03-27 00:03:08 +0700

    add top subscribe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable}`}>
       <body className={cn(GeistSans.variable)}>{children}</body>
     </html>
   );
--- src/components/Homepage/HeroBanner/hero-banner.tsx
+++ src/components/Homepage/HeroBanner/hero-banner.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import classes from "./hero-banner.module.scss";
+import TopSubscribe from "@/components/UI/Top-Subscribe/top-subscribe";
 
 export default function HeroBanner() {
   return (
     <header id="hero-section" >
       <div className={classes.heroBanner}>
+        <TopSubscribe />
         {/* <Image src="/assets/img/Lou_F&B-11.jpg" width={1900} height={1900} alt="Luo-Hero-Banner" priority /> */}
         </div>
     </header>

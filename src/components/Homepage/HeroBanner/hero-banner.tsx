70633b3c1c241d47c3cee42b60b88a493e28106e
itsaFan <steffansim@gmail.com>
2024-03-27 00:15:19 +0700

    testing shadcn nav

--- src/components/Homepage/HeroBanner/hero-banner.tsx
+++ src/components/Homepage/HeroBanner/hero-banner.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import classes from "./hero-banner.module.scss";
 import TopSubscribe from "@/components/UI/Top-Subscribe/top-subscribe";
+import { NavigationMenuDemo } from "@/components/Layout/test-demo";
 
 export default function HeroBanner() {
   return (
     <header id="hero-section" >
       <div className={classes.heroBanner}>
         <TopSubscribe />
+        <NavigationMenuDemo />
         {/* <Image src="/assets/img/Lou_F&B-11.jpg" width={1900} height={1900} alt="Luo-Hero-Banner" priority /> */}
         </div>
     </header>

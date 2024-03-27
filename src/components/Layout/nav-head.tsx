da77232bf63117792be02bc9b7136ae19d0ce4eb
itsaFan <steffansim@gmail.com>
2024-03-27 23:54:11 +0700

    add navhead

--- src/components/Layout/nav-head.tsx
+++ src/components/Layout/nav-head.tsx
@@ -1,10 +1,16 @@
-
+import TopLogo from "./top-logo";
+import { TopNavigationMenu } from "./top-nav-menu";
 
 export default function NavHeader() {
   return (
-
-<div>
-    
-</div>
+    <div className="flex justify-between mx-10 mt-2.5">
+      <div className="flex items-center text-center">
+        <TopLogo />
+      </div>
+      <div className="">
+        <TopNavigationMenu />
+      </div>
+      <div></div>
+    </div>
   );
 }
---
+++ src/components/Layout/top-logo.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function TopLogo() {
+  return <Image src="/assets/img/Lou-transparent.png" width={50} height={50} alt="Luo-Logo" priority />;
+}

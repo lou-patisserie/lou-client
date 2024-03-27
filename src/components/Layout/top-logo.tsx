28b4dc11d14fa0c9bafe176a6042bfe590f462a9
itsaFan <steffansim@gmail.com>
2024-03-28 00:09:40 +0700

    change favicon & metadata title

--- src/components/Layout/top-logo.tsx
+++ src/components/Layout/top-logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function TopLogo() {
-  return <Image src="/assets/img/Lou-transparent.png" width={50} height={50} alt="Luo-Logo" priority />;
+  return <Image src="/assets/img/Lou-transparent-light.png" width={50} height={50} alt="Luo-Logo" priority />;
 }

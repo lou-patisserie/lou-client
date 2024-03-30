c65e97221afbfdd610215dd83aca10afeb2ebe4d
itsaFan <steffansim@gmail.com>
2024-03-30 20:58:38 +0700

    add navbar animation scroll

--- src/components/Layout/nav-head.tsx
+++ src/components/Layout/nav-head.tsx
@@ -1,13 +1,40 @@
-import TopLogo from "./top-logo";
+"use client";
+import { useEffect, useState } from "react";
+
 import { TopNavigationMenu } from "./top-nav-menu";
+import { TopLogo, TopScrolledLogo } from "./top-logo";
 
 export default function NavHeader() {
+  const [showNav, setShowNav] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  console.log(showNav);
+  console.log(lastScrollY);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY <= 150) {
+        setShowNav(false);
+      } else if (currentScrollY > lastScrollY) {
+        setShowNav(false);
+      } else {
+        setShowNav(true);
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <div className="flex justify-between mx-10 mt-4">
-      <div className="flex items-center text-center">
-        <TopLogo />
-      </div>
-      <div className="">
+    <div className={`flex justify-between items-center px-10  transition-transform duration-300 ${showNav ? "fixed z-20 top-0  w-full translate-y-0 bg-luoBiege mt-0 py-2" : "relative -translate-y-full w-full top-12 mt-4"}`}>
+      <div className="flex items-center text-center ">{showNav ? <TopScrolledLogo /> : <TopLogo />}</div>
+      <div>
         <TopNavigationMenu />
       </div>
       <div></div>
--- src/components/Layout/top-logo.tsx
+++ src/components/Layout/top-logo.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
-export default function TopLogo() {
+export function TopLogo() {
   return <Image src="/assets/img/Lou-transparent-light.png" width={50} height={50} alt="Luo-Logo" priority />;
+}
+
+
+export function TopScrolledLogo() {
+  return <Image src="/assets/img/Lou-transparent.png" width={40} height={40} alt="Luo-Logo" priority />;
 }

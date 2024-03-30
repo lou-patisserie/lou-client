f89d697b659f34260e2478aabbd6bdfacb0cbdcf
itsaFan <steffansim@gmail.com>
2024-03-30 21:12:36 +0700

    add smooth behavior scroll

--- src/components/Layout/nav-head.tsx
+++ src/components/Layout/nav-head.tsx
@@ -7,8 +7,8 @@ import { TopLogo, TopScrolledLogo } from "./top-logo";
 export default function NavHeader() {
   const [showNav, setShowNav] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
-  console.log(showNav);
-  console.log(lastScrollY);
+  // console.log(showNav);
+  // console.log(lastScrollY);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,7 +32,7 @@ export default function NavHeader() {
   }, [lastScrollY]);
 
   return (
-    <div className={`flex justify-between items-center px-10  transition-transform duration-300 ${showNav ? "fixed z-20 top-0  w-full translate-y-0 bg-luoBiege mt-0 py-2" : "relative -translate-y-full w-full top-12 mt-4"}`}>
+    <div className={`flex justify-between items-center px-10  transition-transform duration-500  ${showNav ? "fixed z-20 top-0  w-full translate-y-0 bg-luoBiege mt-0 py-2" : "relative -translate-y-full w-full top-12 mt-4"}`}>
       <div className="flex items-center text-center ">{showNav ? <TopScrolledLogo /> : <TopLogo />}</div>
       <div>
         <TopNavigationMenu />

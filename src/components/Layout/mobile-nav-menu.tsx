d5fe98fa2393a6da36597a62417e2c70a4c5d2b0
itsaFan <steffansim@gmail.com>
2024-03-31 20:20:21 +0700

    fix footer

--- src/components/Layout/mobile-nav-menu.tsx
+++ src/components/Layout/mobile-nav-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
-import { ArrowDownNarrowWide, CircleDot, Dot, SquareMenu, SquareX } from "lucide-react";
+import { ArrowDownNarrowWide, Dot, SquareMenu, SquareX } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 

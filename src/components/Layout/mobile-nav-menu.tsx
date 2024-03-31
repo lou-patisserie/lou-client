a967f27ee981ec5e849a850759ebe237dfafdcf4
itsaFan <steffansim@gmail.com>
2024-03-31 20:11:36 +0700

    add responsive navbar

---
+++ src/components/Layout/mobile-nav-menu.tsx
@@ -0,0 +1,149 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { ArrowDownNarrowWide, CircleDot, Dot, SquareMenu, SquareX } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+
+type Props = {
+  showNav: boolean;
+};
+
+export default function MobileNavigationMenu({ showNav }: Props) {
+  const [isSidenavOpen, setIsSidenavOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const sidenavRef = useRef<HTMLDivElement>(null);
+
+  const toggleSidenav = () => {
+    setIsSidenavOpen(!isSidenavOpen);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    const target = event.target as Node;
+    if (sidenavRef.current && !sidenavRef.current.contains(target)) {
+      setIsSidenavOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const sidenavVariants = {
+    open: { transform: "translateX(0%)" },
+    closed: { transform: "translateX(-100%)" },
+  };
+
+  const dropdownVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      display: "block",
+      transition: {
+        duration: 0.3,
+        ease: "easeOut",
+      },
+    },
+    closed: {
+      opacity: 0,
+      y: "-10%",
+      transition: {
+        duration: 0.2,
+        ease: "easeIn",
+
+        transitionEnd: {
+          display: "none",
+        },
+      },
+    },
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={toggleSidenav} className={`text-luoDarkBiege hover:text-slate-700 transition ease-in-out duration-200`}>
+          <SquareMenu size={30} />
+        </button>
+      </div>
+
+      <motion.div
+        ref={sidenavRef}
+        initial="closed"
+        animate={isSidenavOpen ? "open" : "closed"}
+        variants={sidenavVariants}
+        transition={{ type: "tween", duration: 0.3 }}
+        className={`fixed left-0 z-40 w-[60%] h-screen px-4 py-2 overflow-y-auto bg-luoBiege ${showNav ? "top-0" : " -top-6"}`}
+        tabIndex={-1}
+      >
+        <div className="flex gap-4 justify-between pt-2 px-4">
+          <button onClick={toggleSidenav} className="text-luoDarkBiege hover:text-white transition ease-in-out duration-200">
+            <SquareX />
+          </button>
+          <Link href={"#"}>
+            <Image src="/assets/img/Lou-bg-dark.png" alt="Website logo" width={40} height={40} className="" priority />
+          </Link>
+        </div>
+        <div className="py-4 overflow-y-auto mt-2">
+          <ul className="space-y-2 font-bold tracking-wide text-base">
+            <li>
+              <Link href="#" className="flex items-center p-2 rounded-lg text-luoDarkBiege  hover:bg-luoDarkBiege hover:text-white transition ease-in-out duration-200 group">
+                <span className="ms-3">Home</span>
+              </Link>
+            </li>
+            <li onClick={toggleDropdown} onMouseEnter={toggleDropdown} onMouseLeave={() => setIsDropdownOpen(false)} className="cursor-pointer">
+              <span className="flex items-center p-2 rounded-lg text-luoDarkBiege hover:bg-luoDarkBiege hover:text-white transition ease-in-out duration-200 group">
+                <span className="flex-1 ms-3 whitespace-nowrap"> Products</span>
+                <ArrowDownNarrowWide />
+              </span>
+              {isDropdownOpen && (
+                <motion.ul className="flex-1 ms-3 whitespace-nowrap mt-2 text-sm" initial="closed" animate={isDropdownOpen ? "open" : "closed"} variants={dropdownVariants}>
+                  <li className="flex items-center p-2 rounded-lg text-luoDarkBiege hover:bg-luoDarkBiege hover:text-white transition ease-in-out duration-200 group">
+                    <Dot />
+                    Whole Cakes
+                  </li>
+                  <li className="flex items-center p-2 rounded-lg text-luoDarkBiege hover:bg-luoDarkBiege hover:text-white transition ease-in-out duration-200 group">
+                    <Dot />
+                    Gelatto
+                  </li>
+                  <li className="flex items-center p-2 rounded-lg text-luoDarkBiege hover:bg-luoDarkBiege hover:text-white transition ease-in-out duration-200 group">
+                    <Dot />
+                    Others
+                  </li>
+                </motion.ul>
+              )}
+            </li>
+            <li>
+              <Link href="#" className="flex items-center p-2 rounded-lg text-luoDarkBiege hover:bg-luoDarkBiege hover:text-white transition ease-in-out duration-200 group">
+                <span className="ms-3">About Us</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="flex items-center p-2 rounded-lg text-luoDarkBiege hover:bg-luoDarkBiege hover:text-white transition ease-in-out duration-200 group">
+                <span className="ms-3">Our Location</span>
+              </Link>
+            </li>
+          </ul>
+          <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-white">
+            <li>
+              <Link href="#" className="flex items-center p-2 rounded-lg text-luoDarkBiege hover:bg-luoDarkBiege hover:text-white transition ease-in-out duration-200 group">
+                <span className="ms-3">FAQ</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="flex items-center p-2 rounded-lg text-luoDarkBiege hover:bg-luoDarkBiege hover:text-white transition ease-in-out duration-200 group">
+                <span className="ms-3">Privacy</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </motion.div>
+    </>
+  );
+}
--- src/components/Layout/nav-head.tsx
+++ src/components/Layout/nav-head.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 
 import { TopNavigationMenu } from "./top-nav-menu";
 import { TopLogo, TopScrolledLogo } from "./top-logo";
+import MobileNavigationMenu from "./mobile-nav-menu";
+import { ShoppingCart } from "lucide-react";
 
 export default function NavHeader() {
   const [showNav, setShowNav] = useState(false);
@@ -33,11 +35,23 @@ export default function NavHeader() {
 
   return (
     <div className={`flex justify-between items-center px-10  transition-transform duration-500  ${showNav ? "fixed z-20 top-0  w-full translate-y-0 bg-luoBiege mt-0 py-2 " : "relative -translate-y-full w-full top-12 mt-4 "}`}>
-      <div className="flex items-center text-center ">{showNav ? <TopScrolledLogo /> : <TopLogo />}</div>
-      <div>
-        <TopNavigationMenu />
+      <div className="hidden md:flex items-center text-center ">{showNav ? <TopScrolledLogo /> : <TopLogo />}</div>
+      <div className="flex md:hidden">
+        <MobileNavigationMenu showNav={showNav} />
       </div>
-      <div></div>
+      <div>
+        <div className="hidden md:block">
+          <TopNavigationMenu />
+        </div>
+        <div>
+          <div className="flex items-center text-center md:hidden ">
+            <TopScrolledLogo />
+          </div>
+        </div>
+      </div>
+      <div>
+        <ShoppingCart />
+      </div>
     </div>
   );
 }

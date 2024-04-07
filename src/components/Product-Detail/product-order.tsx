2cceaffaf5c77ae075ffc6585306ec7e87a1c315
itsaFan <steffansim@gmail.com>
2024-04-07 21:35:15 +0700

    fix styling

--- src/components/Product-Detail/product-order.tsx
+++ src/components/Product-Detail/product-order.tsx
@@ -6,7 +6,7 @@ import { product } from "./dummy";
 
 export default function ProductOrder() {
   return (
-    <div className="w-full max-w-lg flex flex-col gap-10 mx-4 lg:mx-0">
+    <div className="w-full max-w-lg flex flex-col gap-8 mx-4 lg:mx-0">
       <div className="flex flex-col h-fit items-start justify-start text-luoDarkBiege">
         <h1 className="font-bold text-xl">{product.name}</h1>
         <h2 className="font-bold text-lg tracking-wide">{formatPrice(product.price)}</h2>
@@ -15,7 +15,7 @@ export default function ProductOrder() {
           <span className="uppercase text-xs font-semibold tracking-wide">Order before 12AM to Receive it Next Day</span>
         </div>
       </div>
-      <div className="flex w-full flex-col h-fit items-start justify-start gap-2">
+      <div className="flex w-full flex-col h-fit items-start justify-start gap-4">
         <span className="font-semibold text-luoDarkBiege">Make Your Order Here:</span>
         <OrderForm />
       </div>

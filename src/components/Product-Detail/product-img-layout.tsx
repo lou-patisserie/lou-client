f58395e2e587b07984a1c5fc72e49d7d724ca65d
itsaFan <steffansim@gmail.com>
2024-04-08 00:28:54 +0700

    set eye on hovered

--- src/components/Product-Detail/product-img-layout.tsx
+++ src/components/Product-Detail/product-img-layout.tsx
@@ -2,10 +2,14 @@
 import Image from "next/image";
 import { useState } from "react";
 import ProductImageView from "./product-img-view";
+import { Eye } from "lucide-react";
 
 export default function ProductDetailImgs() {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedImg, setSelectedImg] = useState("");
+  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
+  const [isHovered3, setIsHovered3] = useState(false);
 
   const handleImageClick = (src: string) => {
     setSelectedImg(src);
@@ -14,16 +18,34 @@ export default function ProductDetailImgs() {
 
   return (
     <>
-      <div className="max-w-lg h-full mx-4 md:mx-0 flex flex-col gap-2 ">
-        <div onClick={() => handleImageClick("/assets/dummy/Lou_Croissant2.jpg")} className="flex h-fit  items-start justify-center transition ease-in duration-150 hover:opacity-70">
+      <div className="max-w-lg h-full mx-4 md:mx-0 flex flex-col gap-2 cursor-pointer">
+        <div
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onClick={() => handleImageClick("/assets/dummy/Lou_Croissant2.jpg")}
+          className="flex h-fit relative items-start justify-center transition ease-in duration-150 hover:opacity-70"
+        >
           <Image src="/assets/dummy/Lou_Croissant2.jpg" width={900} height={900} alt="Luo Croissant" className="object-cover aspect-square rounded-lg shadow-lg" />
+          {isHovered && <Eye size={35} className="text-white absolute bottom-2 right-2" />}
         </div>
         <div className="flex flex-row gap-2">
-          <div onClick={() => handleImageClick("/assets/dummy/Lou_Croissant3.jpg")} className="flex h-fit items-start justify-center transition ease-in duration-150 hover:opacity-70 ">
+          <div
+            onMouseEnter={() => setIsHovered2(true)}
+            onMouseLeave={() => setIsHovered2(false)}
+            onClick={() => handleImageClick("/assets/dummy/Lou_Croissant3.jpg")}
+            className="flex relative h-fit items-start justify-center transition ease-in duration-150 hover:opacity-70 "
+          >
             <Image src="/assets/dummy/Lou_Croissant3.jpg" width={900} height={900} alt="Luo Croissant" className="object-cover aspect-square rounded-lg shadow-lg" />
+            {isHovered2 && <Eye size={25} className="text-white absolute bottom-2 right-2" />}
           </div>
-          <div onClick={() => handleImageClick("/assets/dummy/Lou_Croissant0.jpg")} className="flex h-fit items-start justify-center transition ease-in duration-150 hover:opacity-70 ">
+          <div
+            onMouseEnter={() => setIsHovered3(true)}
+            onMouseLeave={() => setIsHovered3(false)}
+            onClick={() => handleImageClick("/assets/dummy/Lou_Croissant0.jpg")}
+            className="flex relative h-fit items-start justify-center transition ease-in duration-150 hover:opacity-70 "
+          >
             <Image src="/assets/dummy/Lou_Croissant0.jpg" width={900} height={900} alt="Luo Croissant" className="object-cover aspect-square rounded-lg shadow-lg" />
+            {isHovered3 && <Eye size={25} className="text-white absolute bottom-2 right-2" />}
           </div>
         </div>
       </div>

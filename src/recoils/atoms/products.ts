62707239a648d6ae14b3c3028cfd0570d1b8ab3e
itsaFan <steffansim@gmail.com>
2024-04-12 17:35:00 +0700

    add drawer and fix Cart State SSR and Client Rendering

--- src/recoils/atoms/products.ts
+++ src/recoils/atoms/products.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { AtomEffect, atom } from "recoil";
 
 type CartItem = {
   name: string;
@@ -11,7 +11,25 @@ type CartItem = {
   complimentaryMsg: string;
 };
 
+const isClient = typeof window !== 'undefined';
+
+const localStorageEffect =
+  (key: string): AtomEffect<CartItem[]> =>
+  ({ setSelf, onSet }) => {
+    if (isClient) {
+      const savedValue = localStorage.getItem(key);
+      if (savedValue != null) {
+        setSelf(JSON.parse(savedValue));
+      }
+
+      onSet((newValue: CartItem[]) => {
+        localStorage.setItem(key, JSON.stringify(newValue));
+      });
+    }
+  };
+
 export const cartState = atom<CartItem[]>({
   key: "cartState",
   default: [],
+  effects_UNSTABLE: [localStorageEffect("cartState")],
 });

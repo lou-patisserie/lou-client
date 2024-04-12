b36411a954211ef6a56e98e1d829a0c54ff1a885
itsaFan <steffansim@gmail.com>
2024-04-12 18:37:33 +0700

    add link

--- src/components/AllProducts/products-item.tsx
+++ src/components/AllProducts/products-item.tsx
@@ -1,5 +1,6 @@
 import { formatPrice } from "@/lib/formatters";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   name: string;
@@ -11,7 +12,16 @@ export default function ProductsItem({ name, price, imgRef }: Props) {
   return (
     <div className="text-center">
       <div className="">
-        <Image src={imgRef} alt="Products" width={500} height={500} priority className={`aspect-square object-cover rounded-md shadow-sm cursor-pointer transition ease-in-out duration-200`} />
+        <Link href="/products/test">
+          <Image
+            src={imgRef}
+            alt="Products"
+            width={500}
+            height={500}
+            priority
+            className={`aspect-square object-cover rounded-md shadow-sm cursor-pointer transition ease-in-out duration-200 hover:opacity-60 hover:border-2 hover:border-luoDarkBiege`}
+          />
+        </Link>
       </div>
       <h3 className="text-lg font-semibold mt-2 text-slate-700 cursor-default">{name}</h3>
       <p className="text-sm mt-1 text-luoDarkBiege cursor-default">{formatPrice(price)}</p>

232f6eb6eb5c53211a69968e47e15fde3cfc457c
itsaFan <steffansim@gmail.com>
2024-04-21 01:51:32 +0700

    initialize about us

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import SubHeroBanner from "@/components/UI/SubHero-Banner/subhero-banner";
+
+export default function AboutUsPage() {
+  return (
+    <>
+      <SubHeroBanner title="About Us" />
+    </>
+  );
+}

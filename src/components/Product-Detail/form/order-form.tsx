8a6e886b5b604df38cda18554c229807e325dedc
itsaFan <steffansim@gmail.com>
2024-06-08 20:55:53 +0700

    fix date

--- src/components/Product-Detail/form/order-form.tsx
+++ src/components/Product-Detail/form/order-form.tsx
@@ -5,7 +5,7 @@ import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
+import { startOfDay } from "date-fns";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/UI/form";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/UI/popover";
 import { cn } from "@/lib/utils";
@@ -138,7 +138,7 @@ export default function OrderForm({ id, name = "", price, selectedVariantName, i
                     </FormControl>
                   </PopoverTrigger>
                   <PopoverContent className="w-auto p-0 rounded-none" align="start">
-                    <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < new Date()} initialFocus />
+                    <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => startOfDay(date) < startOfDay(new Date())} initialFocus />
                   </PopoverContent>
                 </Popover>
                 <FormMessage />

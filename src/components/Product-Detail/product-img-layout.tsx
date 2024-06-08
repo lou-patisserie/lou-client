4c96de456b618152fd7d0ee083417675cefe1db5
itsaFan <steffansim@gmail.com>
2024-06-08 19:57:54 +0700

    img loading screen

--- src/components/Product-Detail/product-img-layout.tsx
+++ src/components/Product-Detail/product-img-layout.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import ProductImageView from "./product-img-view";
 import { Eye } from "lucide-react";
 import { validateImageUrl } from "@/lib/imgUtils";
+import { Skeleton } from "../UI/skeleton";
 
 type Props = {
   mainImg?: string;
@@ -36,38 +37,26 @@ export default function ProductDetailImgs({ mainImg, subImg1, subImg2 }: Props) 
           onClick={() => mainImg && handleImageClick(mainImg)}
           className="flex h-fit relative items-start justify-center transition ease-in duration-150 hover:opacity-70"
         >
-          {mainImg ? (
-            <Image src={validateImageUrl(mainImg)} width={900} height={900} alt="Luo Croissant" className="object-cover aspect-square shadow-lg" onError={handleImgError} />
-          ) : (
-            <div className="flex items-center justify-center w-full h-full bg-gray-200">Loading...</div>
-          )}
+          {mainImg ? <Image src={validateImageUrl(mainImg)} width={900} height={900} alt="Luo Croissant" className="object-cover aspect-square shadow-lg w-full" onError={handleImgError} /> : <Skeleton className="aspect-square w-full" />}
           {isHovered && mainImg && <Eye size={35} className="text-white absolute bottom-2 right-2" />}
         </div>
-        <div className="flex flex-row gap-2">
+        <div className="flex flex-row gap-2 w-full">
           <div
             onMouseEnter={() => setIsHovered2(true)}
             onMouseLeave={() => setIsHovered2(false)}
             onClick={() => subImg1 && handleImageClick(subImg1)}
-            className="flex relative h-fit items-start justify-center transition ease-in duration-150 hover:opacity-70 "
+            className="flex relative h-fit items-start justify-center transition ease-in duration-150 hover:opacity-70 w-full"
           >
-            {subImg1 ? (
-              <Image src={validateImageUrl(subImg1)} width={900} height={900} alt="Luo Croissant" className="object-cover aspect-square shadow-lg" onError={handleImgError} />
-            ) : (
-              <div className="flex items-center justify-center w-full h-full bg-gray-200">Loading...</div>
-            )}
+            {subImg1 ? <Image src={validateImageUrl(subImg1)} width={900} height={900} alt="Luo Croissant" className="object-cover aspect-square shadow-lg w-full" onError={handleImgError} /> : <Skeleton className="aspect-square w-full " />}
             {isHovered2 && subImg1 && <Eye size={25} className="text-white absolute bottom-2 right-2" />}
           </div>
           <div
             onMouseEnter={() => setIsHovered3(true)}
             onMouseLeave={() => setIsHovered3(false)}
             onClick={() => subImg2 && handleImageClick(subImg2)}
-            className="flex relative h-fit items-start justify-center transition ease-in duration-150 hover:opacity-70 "
+            className="flex relative h-fit items-start justify-center transition ease-in duration-150 hover:opacity-70 w-full"
           >
-            {subImg2 ? (
-              <Image src={validateImageUrl(subImg2)} width={900} height={900} alt="Luo Croissant" className="object-cover aspect-square shadow-lg" onError={handleImgError} />
-            ) : (
-              <div className="flex items-center justify-center w-full h-full bg-gray-200">Loading...</div>
-            )}
+            {subImg2 ? <Image src={validateImageUrl(subImg2)} width={900} height={900} alt="Luo Croissant" className="object-cover aspect-square shadow-lg w-full" onError={handleImgError} /> : <Skeleton className="aspect-square w-full" />}
             {isHovered3 && subImg2 && <Eye size={25} className="text-white absolute bottom-2 right-2" />}
           </div>
         </div>

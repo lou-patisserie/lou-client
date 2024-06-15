6bfcb837e3fb7019093d8ec4e2723025cd425c0d
itsaFan <steffansim@gmail.com>
2024-06-15 17:29:59 +0700

    change testimonials

--- src/components/Homepage/Testimony/testimonials.tsx
+++ src/components/Homepage/Testimony/testimonials.tsx
@@ -10,7 +10,7 @@ export default function Testimonials() {
   const plugin = React.useRef(Autoplay({ delay: 2000, stopOnInteraction: false, stopOnMouseEnter: true }));
 
   return (
-    <div className="w-full flex justify-center my-12 px-4 md:px-0">
+    <div className="w-full flex justify-center my-16 md:my-24 px-4 md:px-0">
       <Carousel
         opts={{
           align: "start",

8b33bd9d16c26b403a8aa66f653aa7fe245ec244
Muhammad Avicena <cenarahmant.dev@gmail.com>
2024-06-17 19:35:54 +0700

    chore: set up prod dev url api

--- src/api/api-config.ts
+++ src/api/api-config.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 // baseURL: "http://localhost:3001/api/v1",
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: "https://api-dev.loupatisserie.com/api/v1",
 });

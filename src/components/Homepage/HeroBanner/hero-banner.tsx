12a74e55b75ed1e53011dc9c569a9f8767579dd7
Steffansim <steffansim@egogohub.com>
2024-07-11 14:32:17 +0700

    fix banner text

--- src/components/Homepage/HeroBanner/hero-banner.tsx
+++ src/components/Homepage/HeroBanner/hero-banner.tsx
@@ -16,9 +16,9 @@ export default function HeroBanner() {
         <div className="absolute inset-x-0 bottom-0">
           <div className="max-w-full w-full flex flex-col gap-2 text-center justify-center mb-16">
             <div className="flex justify-center items-center cursor-default">
-              <h1 className="scroll-m-20 drop-shadow-md text-luoDarkBiege md:text-white border-b-2 border-luoDarkBiege pb-2 text-3xl font-semibold tracking-tight first:mt-0 ">Delivery & Pickup</h1>
+              <h1 className="scroll-m-20 drop-shadow-md text-white border-b-2 border-luoDarkBiege pb-2 text-3xl font-semibold tracking-tight first:mt-0 ">Delivery & Pickup</h1>
             </div>
-            <span className="text-luoDarkBiege md:text-white text-lg font-semibold cursor-default drop-shadow-md">Tangerang & Jakarta</span>
+            <span className="text-white text-lg font-semibold cursor-default drop-shadow-md">Tangerang & Jakarta</span>
             <div className="flex justify-center">
               <Button onClick={exploreBtn} className="flex drop-shadow-md justify-center items-center bg-luoDarkBiege rounded-none border-none hover:bg-[#a58b73]">
                 Explore Our Products

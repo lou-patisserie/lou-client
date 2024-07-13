ef36ead9a209f396f934fb8dad909d6cd02d55b5
itsaFan <steffansim@gmail.com>
2024-07-13 21:54:14 +0700

    fix navbar/layout

--- src/components/UI/navigation-menu.tsx
+++ src/components/UI/navigation-menu.tsx
@@ -6,7 +6,7 @@ import { ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 const NavigationMenu = React.forwardRef<React.ElementRef<typeof NavigationMenuPrimitive.Root>, React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>>(({ className, children, ...props }, ref) => (
-  <NavigationMenuPrimitive.Root ref={ref} className={cn("relative z-10 flex max-w-max flex-1 items-center justify-center", className)} {...props}>
+  <NavigationMenuPrimitive.Root ref={ref} className={cn("relative z-50 flex max-w-max flex-1 items-center justify-center", className)} {...props}>
     {children}
     <NavigationMenuViewport />
   </NavigationMenuPrimitive.Root>
